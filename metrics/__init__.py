ab774935ee852f66aa5fb0910822540cfc0fd02a
Jonathan Gardner <jgardner@jonathangardner.net>
2010-11-27 14:07:19 -0800

    Some cleanup and documentation

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,24 +1,56 @@
 """Logging facilities for metrics.
 
-Metrics represent instaneous event and their attached data, or events that
-take a duration.
+This module is used for recording, at a much higher-grain level than
+performance analyzing software, the large-scale behavior of your system. For
+instance, let's say you run a script every 5 minutes and you want to know how
+long it takes for each step, how many records are manipulated, and what the
+sum totals are for each operation. Or, you run a website and you want to know
+how long each request takes, as well as where each request originated from in
+addition to the session ID or user ID of the request.
 
-Each event has the following mandatory attributes:
+To record metrics, you must first setup a METRICS CONTEXT. This is an object
+that records numbers and times for the entire operation. Contexts are not
+nestable, so make sure you only setup one context for the entire operation.
+This could be a single request, an entire run of a thread, or a complete
+script execution.
 
-    - context: The context of the event.
+To setup a metrics context, simply do:
 
-    - start: The time when it started, in seconds since epoch. (As returned by
-      time.time()
+    import metrics
 
-    - name: A unique name to identify the event. This name is really more of a
-      type. Within a context, an event name need not be unique.
+    with Context() as c:
+        ...
 
-    - misc. attributes: These attributes may include things like "duration"
-      which records how long the event took.
+Note that contexts take name-value pairs, which you can pass in the
+constructor or set with the 'c' object, which is a dict.
 
-Contexts describe the overall context of an event. This would include things
-like important request data and such. All events occur within a specific
-context.
+Once you have a context setup, you can record events. There are, roughly, two
+types of events:
+
+    * Instaneous events, occurring at an instance in time.
+    * Duration events, occurring over a period of time.
+
+To record any event, you can use the 'event' method, passing in a name and the
+keywords you want to us. 'start' is always used and records when the event
+started. For instaneous events, there is no 'duration' specified, which is how
+long the event took.
+
+To make recording events with duractions, the 'Timer' class can be used.
+
+    with Timer() as t:
+        ...
+
+The start time is when the timer was created---before the block of code inside
+the with statement is run. Like the Context object, you can specify keywords
+in the constructor of the Timer or by manipulating the 't' dictionary.
+
+If you are using a web app with WSGI, you can use the MetricsContextApp which
+will setup a context for every request that passes through it, setting key
+keywords.
+
+The metrics are output using the logger with the name 'metrics' at log level
+'info'. To parse the logs, extract the JSON data from each line. No parsing
+support is provided with this module.
 """
 import json
 from time import time
@@ -27,6 +59,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
+# The global, threadlocal context
 context = threading.local()
 
 def event(name, **kw):
@@ -36,14 +69,17 @@ def event(name, **kw):
     type.
 
     'start' is set if not specified to the current time.
+
+    Other keyword arguments can be used to taste.
     """
-    kw.setdefault('start', time())
-    kw['name'] = name
     events = getattr(context, 'events', None)
     if events is None:
         log.warn("%s: no context to record event: %r" % (
             threading.current_thread().name, kw))
     else:
+        if 'start' not in kw:
+            kw['start'] = time()
+        kw['name'] = name
         events.append(kw)
 
 class Timer(object):
@@ -58,7 +94,6 @@ class Timer(object):
     The event is registered in the threadlocal context, if any, once it
     completes.
     """
-
     def __init__(self, name, **kw):
         self.name = name
         self.kw = kw
@@ -82,7 +117,7 @@ class Context(object):
         c['session_id'] = session.id
 
     Note that libraries do not need access to the context in the with
-    statement. They will have access through a threadlocal variable.
+    statement. They will have access through a threadlocal global variable.
     """
 
     def __init__(self, **kw):
@@ -91,22 +126,19 @@ class Context(object):
 
     def __enter__(self):
         self.start = time()
-        context.old_context = dict(
-                kw=getattr(context, 'kw', None),
-                events=getattr(context, 'events', None),
-                old_context=getattr(context, 'old_context', None))
+        if getattr(context, 'kw', None):
+            raise ValueError("There is already a context defined")
+
         context.events = self.events
         context.kw = self.kw
-        log.debug("%s: entering new context, replacing %r" % (
-            threading.current_thread().name,
-            context.old_context['kw'],))
+        log.debug("%s: entering new context" % (
+            threading.current_thread().name,))
 
         return self.kw
 
     def __exit__(self, exc_type, exc_value, traceback):
-        context.kw = context.old_context['kw']
-        context.events = context.old_context['events']
-        context.old_context = context.old_context['old_context']
+        del context.kw
+        del context.events
 
         self.kw.update(dict(
             events=self.events,
@@ -118,6 +150,26 @@ class Context(object):
             threading.current_thread().name,))
 
 class MetricsContextApp(object):
+    """A WSGI App that will setup a metrics context around the entire request.
+
+    The context is set to environ['metrics_context'], in case you want to
+    modify it further, such as specifying the user_id or session_id of the
+    request. Usually, you can use "Timer" and "event" without referring to the
+    metrics context since it is a threadlocal global.
+
+    The keywords set for the context are:
+
+        REMOTE_ADDR: The REMOTE_ADDR of environ, namely the IP address of the
+                     HTTP client making the request.
+
+        PATH_INFO:   The PATH_INFO of the environ, namely the path of the
+                     request (without the host.)
+
+        QUERY_STRING: The QUERY_STRING of the environ, namely the GET
+                      arguments to the request. This may or may not be useful.
+
+        thread:       The current thread name.
+    """
     def __init__(self, app):
         self.app = app
 
